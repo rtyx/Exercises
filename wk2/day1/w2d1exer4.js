b82157f272c300662277b28a9c092df7ce02ef29
rtyx <rafaeltoledanoillan@gmail.com>
2016-09-09 13:35:18 +0200

    The kitties are ready to fight (except on mobile phones)

--- wk2/day1/w2d1exer4.js
+++ wk2/day1/w2d1exer4.js
@@ -1,7 +1,9 @@
 var kitten = document.getElementsByClassName("kit");
 var dots = document.getElementsByClassName("dot");
+var anydot = document.getElementById("dots");
 var katout = 0;
 var katin = 1;
+var keepThemKittiesComin;
 
 setTimeout(function carousel() {
     kitten[katout].classList.remove("in");
@@ -9,15 +11,46 @@ setTimeout(function carousel() {
     kitten[katout].classList.add("out");
     kitten[katin].classList.add("in");
     dots[katin].classList.add("activedot");
-    kitten[katout].addEventListener("transitionend", function removeOut(e) {
+    kitten[katout].addEventListener("transitionend", function removeOut() {
         this.removeEventListener("transitionend", removeOut);
-        console.log(e);
         this.classList.remove("out");
-        setTimeout(carousel, 5000);
+    });
+    anydot.addEventListener("click", function() {
+        clearTimeout(keepThemKittiesComin);
     });
     katout = katin;
     katin = katout + 1 >= kitten.length ? 0 : katout + 1;
-}, 5000);
+    keepThemKittiesComin = setTimeout(carousel, 3000);
+}, 3000);
+
+for(var i=0; i< dots.length; i++) {
+    dots[i].addEventListener("click", switchThemKitties(i));
+}
+
+function switchThemKitties(i) {
+    return function() {
+        for (var j=0; j < dots.length; j++) {
+            dots[j].classList.remove("activedot");
+            if (kitten[j].classList.contains("in")) {
+                kitten[j].classList.remove("in");
+                kitten[j].classList.add("out");
+            }
+        }
+        console.log("Cat " + i + ", I choose you!");
+        kitten[i].addEventListener("transitionend", function cleanThemKitties() {
+            this.removeEventListener("transitionend", cleanThemKitties);
+            for (var j=0; j < dots.length; j++) {
+                kitten[j].classList.remove("out");
+            }
+        });
+        dots[i].classList.add("activedot");
+        kitten[i].classList.add("in");
+    };
+}
+
+
+
+
 /*
 function kitinAround() {
   switch (whichDot) {

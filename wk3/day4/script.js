15dd724511864f35647dc2e44f8347039f5b4b2c
rtyx <rafaeltoledanoillan@gmail.com>
2016-09-19 18:06:22 +0200

    Made jQuery version of ticker

--- wk3/day4/script.js
+++ wk3/day4/script.js
@@ -1,17 +1,32 @@
 var textarea = document.getElementById('textarea');
-var errormessage = document.getElementById('errormessage');
+var message = document.getElementById('message');
 
 window.addEventListener("load", textarea.focus());
+
+function itsJSON() {
+    console.log("It's a JSON");
+    textarea.classList.remove("red");
+    textarea.classList.add("green");
+    message.classList.add("okmessage");
+    message.classList.remove("errormessage");
+    message.innerHTML = "More JSON than my son Jason! <br> Good to go kiddo. </br>";
+}
+
+function itsNotJSON(error) {
+    console.log(error);
+    textarea.classList.remove("green");
+    textarea.classList.add("red");
+    message.classList.remove("okmessage");
+    message.classList.add("errormessage");
+    message.innerHTML = "Oops, that doesn't look like a JSON <br>" + error + "</br>";
+    console.log("It's not a JSON");
+}
 
 document.getElementById('checkbutton').addEventListener("click", function checkJSON() {
     try {
         JSON.parse(textarea.value);
-        console.log("It's a JSON");
-        textarea.classList.add("green");
+        itsJSON();
     } catch (error) {
-        console.log(error);
-        textarea.classList.add("red");
-        errormessage.innerHTML = error;
-        console.log("It's not a JSON");
+        itsNotJSON(error);
     }
 });

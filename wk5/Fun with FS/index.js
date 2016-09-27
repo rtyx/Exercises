6ff3cccb1468afeda91b468f8a5955128d81308c
rtyx <rafaeltoledanoillan@gmail.com>
2016-09-27 13:44:08 +0200

    Finished Part 1 of the FS exercise

---
+++ wk5/Fun with FS/index.js
@@ -0,0 +1,36 @@
+var fs = require('fs');
+
+var mypath = __dirname + '/files';
+
+function listAll(path) {
+    fs.readdir(path, function(err, data) {
+        console.log(path + "/ contains " + data);
+        for (var i = 0; i < data.length; i++) {
+            function hello(i) {
+                fs.stat(path + "/" + data[i], function(err, stats) {
+                    if (stats.isDirectory()) {
+                        var newPath = path + "/" + data[i];
+                        listAll(newPath);
+                    }
+                });
+            }
+            hello(i);
+        }
+
+///ALTERNATIVE WITH FOR EACH////
+
+        // data.forEach(function(file) {
+        //     fs.stat(path + "/" + file, function(err, data) {
+        //         if (err) {
+        //             console.log(err);
+        //         }
+        //         if (data.isDirectory()) {
+        //             var newPath = path + "/" + file;
+        //             listAll(newPath);
+        //         }
+        //     });
+        // });
+    });
+}
+
+listAll(mypath);
